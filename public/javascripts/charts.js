cc064f53a1829614764615443ccc9644b209812c
Umran Hussein <umran.hussein@gmail.com>
2016-09-29 23:45:28 -0700

    graphs!

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,4 +1,4 @@
-var socket = io('http://127.0.0.1:3006')
+var socket = io('http://127.0.0.1:3006/feed')
 
 var graphs = []
 for(i=0; i<27; i++) {
@@ -10,30 +10,21 @@ socket.on('connect', function(){
 	console.log("connected to server")
 })
 
-socket.on('startSession', function(){
-	console.log("session initialized. requesting tick data")
-	nextTick()
-})
-
 socket.on('tick', function(data){
 	console.log(data)
 	
-	for(i=0; i<data.length; i++){
+	for(i=0; i<27; i++){
 		var pair = data[i]
 		graphs[i][0].x.push(pair.timestamp)
-		graphs[i][0].y.push(pair.ask_fig + pair.ask_pts)
-	
-		Plotly.newPlot('chart'+i, graphs[i])
+		graphs[i][0].y.push(pair.bid_fig + pair.bid_pts)
+		
+		// for the time being only plot the graph of usd/cad
+		if(pair.pair === "USD/CAD" || pair.pair === "AUD/CAD") {
+			Plotly.newPlot('chart'+i, graphs[i])
+		}
 	}
-	
-	console.log("next tick in 1 second...")
-	setTimeout(nextTick, 5000)
 })
 
 socket.on('disconnect', function(){
 	console.log("disconnected from server")
 })
-
-function nextTick() {
-	socket.emit('nextTick')
-}
